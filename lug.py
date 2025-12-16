5fd492e0e46f5236e17a971114711109055b3e11
Dmytro <gldima1208@gmail.com>
2025-12-16 16:21:47 +0100

    bgcn

--- lug.py
+++ lug.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import sin, cos
+from numpy import sin, cos,pi
 
 class Material:
     """
@@ -12,9 +12,29 @@ class Material:
         rho (float): Density [kg/m^3].
     """
 
-    def __init__(self, Name=None, E=0, y_stress=0, alpha=1, rho=1):
+    def __init__(self, Name=None, E=0, y_stress=0, alpha=1, rho=1,nu=0.5):
         self.Name = Name
         self.E = E
         self.y_stress = y_stress
         self.alpha = alpha
         self.rho = rho
+        self.nu = nu
+
+mat_att=Material('ds')
+mat_fast=Material('Material')
+t1=0.003
+F_z=1e3
+F_xy=1e3
+
+def mass(x):
+    a_,b_,t2,w_,D2=x
+    e=1.5*D2
+    a=a_+2*e
+    b=b_+2*e
+    w=w_+2*e+3*D2
+    return mat_att.rho(((a+b+t2)*w-4*pi*D2**2/4)*t2)+4*mat_fast.rho*(t1 + t2 + 0.8 * D2 * 2 * 1.5 ** 2)*(pi*D2**2/4)
+def sandwich_side_failure(x):
+    a_,b_,t2,w_,D2=x
+    return ((F_xy/(D2*t2))**2+3*)**(1/2)
+    
+
