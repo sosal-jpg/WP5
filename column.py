700b439a44dca6fde5635bc1fea0545d364dc686
Dmytro <gldima1208@gmail.com>
2026-01-08 14:16:59 +0100

    fjkasdfkd

--- column.py
+++ column.py
@@ -22,70 +22,116 @@ from numpy import sin, cos,pi
 from scipy.optimize import minimize, NonlinearConstraint
 g=9.8
 
+L_tanks=1.8
+m_tanks=210
+L=L_tanks
+R=0.6
 bounds = [
-    (0.00, 100.00),  # R
-    (0.00, 0.3),  # t1
-    (1.01, 5.0000),  # L
+    (1, 30),  # t1
 ]
-options = {"maxiter": 100, }
-mats_col=[]
+options = {"maxiter": 1000, }
+mats_col=[
+    Material(Name='4130 Steel', E=205e9, y_stress=725e6, alpha=12.6e-6, rho=7850),
+    Material(Name='8630 Steel', E=200e9, y_stress=550e6, alpha=11.7e-6, rho=7850),
+    Material(Name='2014-T6 Aluminium', E=73.1e9, y_stress=414e6, alpha=24.4e-6, rho=2800),
+    Material(Name='2024-T3 Aluminium', E=73.1e9, y_stress=345e6, alpha=24.7e-6, rho=2780),
+    Material(Name='2024-T4 Aluminium', E=73.1e9, y_stress=345e6, alpha=24.7e-6, rho=2780),
+    Material(Name='AZ91C-T6 Magnesium', E=44.8e9, y_stress=145e6, alpha=26e-6, rho=1810),
+    Material(Name='7075-T6 Aluminium', E=73.1e9, y_stress=503e6, alpha=23.6e-6, rho=2810),
+]
+
 mats_fast=[]
 
-sandwich=[[]]
+sandwich=[[6.27+2,0],[37+2,0.6],[7.77+2,1.2],[2,1.8]]
 axial_a=6*g
 transterse_a=2*g
-p=5e5
+p_given=5e5
 #devo iterare per il caso in cui sommo pressione e senza
+
+mass_min=np.inf
+res_min=None
+mat_min=None
+p_min=None
 for mat_col in mats_col:
-    rho_col=mat_col.rho
-    def choose_p(func, p):
-        modified=lambda x: func(x,p)
-        return modified
-    def MaxStress_compressive(x,p):
-        R,t1,L=x
-        A=2*pi*R*t1
-        I=pi*R**3*t1
-        M_bend = mass(x)*transterse_a*L/2
-        F_axial=mass(x)*axial_a-p*pi*R**2
-        stress_hoop=p*R/t1
-        for mass,h in sandwich:
-            M_bend+=mass*transterse_a*h
-            F_axial+=mass*axial_a
-        theta=np.arange(0,91,1)
-        def stress(theta):
-            Q=R**2*t1*sin(theta)
-            stress_z=F_axial/A+M_bend*R/I
-            stress_shear=F_axial*Q/(I*t1)
-            return (stress_z**2+stress_hoop**2-stress_z*stress_hoop+3*stress_shear**2)**(1/2)
-        return max(stress(i) for i in theta)
+    for p in [0,p_given]:
+        rho_col=mat_col.rho
+        def choose_p(func, p):
+            modified=lambda x: func(x,p)
+            return modified
+        def MaxStress(t1,p):
+            t1*=0.001
+            A=2*pi*R*t1
+            I=pi*R**3*t1
+            M_bend = mass(t1)*transterse_a*L/2
+            F_axial=mass(t1)*axial_a-p*pi*R**2
+            stress_hoop=p*R/t1
+            for m,h in sandwich:
+                M_bend+=m*transterse_a*h
+                F_axial+=m*axial_a
+            M_bend+=m_tanks*transterse_a*L_tanks/2
+            stress_z=-F_axial/A
+            return max(((stress_z+M_bend*R/I)**2+stress_hoop**2-(stress_z+M_bend*R/I)*stress_hoop)**(1/2),((stress_z-M_bend*R/I)**2+stress_hoop**2-(stress_z-M_bend*R/I)*stress_hoop)**(1/2))/mat_col.y_stress
+            
+        
+        def MaxStress_compr(t1,p):
+            t1*=0.001
+            A=2*pi*R*t1
+            I=pi*R**3*t1
+            M_bend = mass(t1)*transterse_a*L/2
+            F_axial=mass(t1)*axial_a-p*pi*R**2
+            for m,h in sandwich:
+                M_bend+=m*transterse_a*h
+                F_axial+=m*axial_a
+            M_bend+=m_tanks*transterse_a*L_tanks/2
+            stress_z=max(F_axial/A+M_bend*R/I,0)
+            return stress_z
 
-    def ColumnBuckling(x,p):
-        R,t1,L=x 
-        A = 2 * pi * R * t1
-        I = pi * R**3 * t1
-        Sigma_cb= pi**2 * mat_col.E * I/ (A * L**2)
-        return MaxStress_compressive(x)/Sigma_cb # devo fare divisione con maximal stress nel grafico e devo controllare che questo non superi 1, penso che questo maximal stress derivi da loading 
-    def ShellBuckling(x,p):
-        R,t1,L=x
-        Q= p*R**2/(mat_col.E*t1**2)
-        lamda = np.sqrt((12* L**4 * (1-mat_col.nu**2))/(pi**4 * R**2 * t1**2) )
-        k = lamda + 12* L**4 * (1-mat_col.nu**2) / ( pi**4 * R**2 * t1**2 * lamda)
-        Sigma_sb= (1.983 - 0.983*np.exp(-23.14*Q))*k*pi**2*mat_col.E*t1**2/(12*(1-mat_col.nu**2)*L**2)
-        return MaxStress_compressive(x)/Sigma_sb
-    def mass(x):
-        R,t1,L=x
-        return rho_col*(pi*R*t1*2*L)
+        def ColumnBuckling(t1,p):
+            t1*=0.001 
+            A = 2 * pi * R * t1
+            I = pi * R**3 * t1
+            Sigma_cb= pi**2 * mat_col.E * I/ (A * L**2)
+            return MaxStress_compr(t1,p)/Sigma_cb# devo fare divisione con maximal stress nel grafico e devo controllare che questo non superi 1, penso che questo maximal stress derivi da loading 
+        def ShellBuckling(t1,p):
+            t1*=0.001
+            Q= p*R**2/(mat_col.E*t1**2)
+            lamda = np.sqrt((12* L**4 * (1-mat_col.nu**2))/(pi**4 * R**2 * t1**2) )
+            k = lamda + 12* L**4 * (1-mat_col.nu**2) / ( pi**4 * R**2 * t1**2 * lamda)
+            Sigma_sb= (1.983 - 0.983*np.exp(-23.14*Q))*k*pi**2*mat_col.E*t1**2/(12*(1-mat_col.nu**2)*L**2)
+            return MaxStress_compr(t1,p)/Sigma_sb
+        def mass(x):
+            t1=x
+            t1*=0.001
+            return rho_col*(2*pi*R*t1*L)
     
-    constraints=[]
-    constraints.append(NonlinearConstraint(choose_p(ColumnBuckling,0), lb=1,ub=np.inf))
-    constraints.append(NonlinearConstraint(choose_p(ShellBuckling,0), lb=1,ub=np.inf))
-    constraints.append(NonlinearConstraint(choose_p(MaxStress_compressive,0), lb=0,ub=mat_col.y_stress))
-    constraints.append(NonlinearConstraint(choose_p(ColumnBuckling,p), lb=1,ub=np.inf))
-    constraints.append(NonlinearConstraint(choose_p(ShellBuckling,p), lb=1,ub=np.inf))
-    constraints.append(NonlinearConstraint(choose_p(MaxStress_compressive,p), lb=0,ub=mat_col.y_stress))
-    x0=[1.2,0.003,3]
-    res=minimize(mass, x0=x0, constraints=constraints, bounds=bounds, method='SLSQP', options=options)
+        while True:
+            
+        constraints=[]
+        constraints.append(NonlinearConstraint(choose_p(ColumnBuckling,p), lb=0,ub=1))
+        constraints.append(NonlinearConstraint(choose_p(ShellBuckling,p), lb=0,ub=1))
+        constraints.append(NonlinearConstraint(choose_p(MaxStress,p), lb=0,ub=1))
+        x0=[1.2,3]
+        
+        if res.fun<mass_min:
+            mass_min=res.fun
+            res_min=res
+            mat_min=mat_col
+            p_min=p
 
+x=res_min.x
+mat_col=mat_min
+p=p_min
+print(f"{mat_col.Name}")
+print(res_min)
+print(f'pressure: {p}')
+constraints=[]
+constraints.append(NonlinearConstraint(choose_p(ColumnBuckling,p), lb=0,ub=1))
+constraints.append(NonlinearConstraint(choose_p(ShellBuckling,p), lb=0,ub=1))
+constraints.append(NonlinearConstraint(choose_p(MaxStress,p), lb=0,ub=1))
+print(constraints[0].fun(x))
+print(constraints[1].fun(x))
+print(constraints[2].fun(x))
+print('\n\n')
 
 
   #output è total or cricical stress  
--- lug.py
+++ lug.py
@@ -11,7 +11,6 @@ class Material:
         alpha (float): Thermal Expansion Coefficient [1/K].
         rho (float): Density [kg/m^3].
     """
-
     def __init__(self, Name=None, E=0, y_stress=0, alpha=1, rho=1,nu=0.5):
         self.Name = Name
         self.E = E
@@ -22,19 +21,85 @@ class Material:
 
 mat_att=Material('ds')
 mat_fast=Material('Material')
-t1=0.003
+t_col=0.003
+t_sand=0.003
 F_z=1e3
-F_xy=1e3
+F_x,F_y=1e3
 
 def mass(x):
-    a_,b_,t2,w_,D2=x
-    e=1.5*D2
+    a_,b_,t,w_,D=x
+    e=1.5*D
     a=a_+2*e
     b=b_+2*e
-    w=w_+2*e+3*D2
-    return mat_att.rho(((a+b+t2)*w-4*pi*D2**2/4)*t2)+4*mat_fast.rho*(t1 + t2 + 0.8 * D2 * 2 * 1.5 ** 2)*(pi*D2**2/4)
+    w=w_+2*e+3*D
+    return mat_att.rho(((a+b+t)*w-4*pi*D**2/4)*t)+4*mat_fast.rho*(t_col + t + 0.8 * D * 2 * 1.5 ** 2)*(pi*D**2/4)
 def sandwich_side_failure(x):
-    a_,b_,t2,w_,D2=x
-    return ((F_xy/(D2*t2))**2+3*)**(1/2)
-    
+    a_,b_,t,w_,D=x
+    return ((F_xy/(D*t))**2+3*)**(1/2)
+
+def lug_hole_sand(x):
+    a_,b_,t,w_,D=x
+    s_plane=(F_y**2+F_x**2)**(1/2)/(t*D)
+    s_shear_z=F_z/(pi*D*t)
+    return (s_plane**2+3*s_shear_z)**(1/2)
+
+def sand_hole_sand(x):
+    a_,b_,t,w_,D=x
+    s_plane=(F_y**2+F_x**2)**(1/2)/(t_sand*D)
+    s_shear_z=F_z/(pi*D*t_sand)
+    return (s_plane**2+3*s_shear_z)**(1/2)
+
+def bolt_hole_sand(x):
+    a_,b_,t,w_,D=x
+    A=pi*D**2/4
+    s_shear=(F_y**2+F_x**2)**(1/2)/A
+    s_axial=F_z/A
+    return (s_axial**2+3*s_shear)**(1/2)
+
+def lug_hole_col(x):
+    a_,b_,t,w_,D=x
+    e=1.5*D
+    a=a_+2*e
+    b=b_+2*e
+    w=w_+2*e+3*D
+    F_yz=F_z*(a-e+t/2)/(b-e-t)
+    F_yx=F_x*(a-e+t/2)/(w-2*e)
+    F_zx=F_x*(b-e+t/2)/(w-2*e)
+    s_plane=((F_z+F_zx)**2+F_x**2)**(1/2)/(t*D)
+    s_shear_z=(F_y+F_yz+F_yx)/(pi*D*t)
+    return (s_plane**2+3*s_shear_z)**(1/2)
+
+def col_hole_col(x):
+    a_,b_,t,w_,D=x
+    e=1.5*D
+    a=a_+2*e
+    b=b_+2*e
+    w=w_+2*e+3*D
+    F_yz=F_z*(a-e+t/2)/(b-e-t)
+    F_yx=F_x*(a-e+t/2)/(w-2*e)
+    F_zx=F_x*(b-e+t/2)/(w-2*e)
+    s_plane=((F_z+F_zx)**2+F_x**2)**(1/2)/(t_col*D)
+    s_shear_z=(F_y+F_yz+F_yx)/(pi*D*t_col)
+    return (s_plane**2+3*s_shear_z)**(1/2)
+
+def bolt_hole_col(x):
+    a_,b_,t,w_,D=x
+    e=1.5*D
+    a=a_+2*e
+    b=b_+2*e
+    w=w_+2*e+3*D
+    A=pi*D**2/4
+    F_yz=F_z*(a-e+t/2)/(b-e-t)
+    F_yx=F_x*(a-e+t/2)/(w-2*e)
+    F_zx=F_x*(b-e+t/2)/(w-2*e)
+    s_shear=((F_z+F_zx)**2+F_x**2)**(1/2)/A
+    s_axial=(F_y+F_yz+F_yx)/A
+    return (s_axial**2+3*s_shear)**(1/2)
+
+def bending(x):
+    a_,b_,t,w_,D=x
+    e=1.5*D
+    a=a_+2*e
+    b=b_+2*e
+    w=w_+2*e+3*D
 
